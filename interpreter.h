df206307ed22f2a09539e8e2f11b2703f7fd1027
Rúben Domingues <51157774+rubendomingues@users.noreply.github.com>
2019-11-18 11:21:31 +0000

    Delete interpreter.h

--- interpreter.h
+++
@@ -1,5 +0,0 @@
-void cmdPrint(Cmd* comand, int tab);
-void cmdListPrint(Cmd_list* cmdList, int tab);
-void boolPrint(BoolExpr* expr, int tab);
-void exprPrint(Expr* expr, int tab);
-void tabPrint(int tab);
